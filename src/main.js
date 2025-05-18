53e0b31661b789720c90d8104a980cfb45b15d3f
Wang <wqs@qq.com>
2025-05-18 10:42:53 +0800

    完成基本插件安装和测试

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from '@/router/index' // @ what is it?
+import store from './store/index'   // same . is work
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.use(VueAxios, axios)
+
+app.mount('#app')
---
+++ src/views/YikeIndex.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <p>index of views</p>
+        <p class="title"> Rouderls board</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            aaa: '',
+        }
+    },
+
+    components: {
+
+    },
+
+    computed: {
+
+    },
+
+    created() {
+        this.getUser();
+    },
+
+    methods: {
+        getUser() {
+            this.axios.post("http://182.61.20.123:3001/singin/match", { data: 'aaa', pwd: 'bbb' })
+                .then((res) => {
+                    console.log(res)
+                
+            })
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.title {
+    font-size: 24px;
+}
+</style>
